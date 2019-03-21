bbe20800c1aad80581f60bc540038f51c1479e59
deveron reniers <deveron-reniers@hotmail.com>
2019-03-21 03:30:33 +0100

    pagecontroller update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,77 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                welcome
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p style="height: 200px;">opvallen en als de bliksem</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p style="height: 200px;">gratis visitekaartjes</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p style="height: 250px;">geen onnodige uren betaal voor wat je krijgt</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p style="height: 250px;">alle tools voor een onderneming anno 2019 onder een dak. Van visitekaartjes tot webshops en zoekmachine optimalisatie voor een vriendeke prijs met oog voor design en kwaliteit. Dat is bliksem design</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p style="height: 250px;">geen onnodige uren betaal voor wat je krijgt</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <p style="height: 50px;">visitekaartjes</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <p style="height: 50px;">logo</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <p style="height: 50px;">Video</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 @endsection
 
 @push('css')
+    <style>
+        .panel{
+            border: 0px !important;
+            border-radius: 0px !important;
+        }
+    </style>
 @endpush
 
 @push('js')
